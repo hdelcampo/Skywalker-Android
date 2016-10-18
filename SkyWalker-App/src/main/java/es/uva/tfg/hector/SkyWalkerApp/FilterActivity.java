4561f29dbe4c6ad6ef1ced06b04e0bf1c3ffac1c
hecdelc <hector.campo@alumnos.uva.es>
2016-10-18 16:17:00 +0200

    Final style for Filter dialog
    +Moved to new, better implementation.
    +Build moved to #13

--- SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/FilterActivity.java
+++ SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/FilterActivity.java
@@ -3,11 +3,13 @@ package es.uva.tfg.hector.SkyWalkerApp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +21,16 @@ import java.util.List;
 /**
  * Dialog to filter {@Code PointsOfInterest}.
  */
-public class FilterActivity extends Activity {
+public class FilterActivity extends Activity implements View.OnClickListener {
 
+    /**
+     * Activity's identifier tag
+     */
+    private static String TAG = "Filter dialog";
+
+    /**
+     * List of items that were being already used, and list of all items to represent.
+     */
     private List<String> usedPoints,
                         allPoints;
 
@@ -32,38 +42,43 @@ public class FilterActivity extends Activity {
         allPoints = getIntent().getStringArrayListExtra("allPoints");
         addPoints();
         Button acceptBtn = (Button) findViewById(R.id.acceptButton);
-        acceptBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent returnIntent = new Intent();
-                returnIntent.putStringArrayListExtra("selected", (ArrayList<String>) getSelectedPoints());
-                setResult(RESULT_OK, returnIntent);
-                finish();
-            }
-        });
+        acceptBtn.setOnClickListener(this);
+
+        Button selectBtn = (Button) findViewById(R.id.selectButton);
+        selectBtn.setOnClickListener(this);
+
+        Button deselectBtn = (Button) findViewById(R.id.deselectButton);
+        deselectBtn.setOnClickListener(this);
+
+        Button cancelBtn = (Button) findViewById(R.id.cancelButton);
+        cancelBtn.setOnClickListener(this);
     }
+
+
 
     /**
      * Adds points to show, and selects their CheckBoxes if they were already being shown.
      */
     private void addPoints() {
-        LinearLayout checkList = (LinearLayout) findViewById(R.id.pointsList);
+        ListView itemsList = (ListView) findViewById(R.id.itemsList);
+        itemsList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
 
-        for (String point: allPoints) {
-            CheckBox listElement = new CheckBox(this);
-            listElement.setChecked(false);
-            listElement.setText(point);
-            View row = getLayoutInflater().inflate(R.layout.filter_row, null);
-            TextView id = (TextView) row.findViewById(R.id.rowText);
-            id.setText(point);
+        String[] from = new String[allPoints.size()];
+        from = allPoints.toArray(from);
+        final int[] to = {android.R.id.text1};
 
-            if (usedPoints.contains(point)) {
-                CheckBox checkBox = (CheckBox) row.findViewById(R.id.checkBox);
-                checkBox.setChecked(true);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_multiple_choice,
+                from);
+
+        itemsList.setAdapter(adapter);
+
+        for(int i = 0; i < adapter.getCount(); i++) {
+            if (usedPoints.contains(adapter.getItem(i))) {
+                itemsList.setItemChecked(i, true);
             }
-
-            checkList.addView(row);
         }
+
     }
 
     /**
@@ -72,12 +87,12 @@ public class FilterActivity extends Activity {
      */
     private List<String> getSelectedPoints() {
         List<String> selecteds = new ArrayList<>();
-        LinearLayout checkList = (LinearLayout) findViewById(R.id.pointsList);
+        final ListView itemsList = (ListView) findViewById(R.id.itemsList);
 
-        for (int i = 0; i < checkList.getChildCount(); i++) {
-            View row = checkList.getChildAt(i);
-            CheckBox checkBox = (CheckBox) row.findViewById(R.id.checkBox);
-            if (checkBox.isChecked()) {
+        final SparseBooleanArray checked = itemsList.getCheckedItemPositions();
+
+        for (int i = 0; i < itemsList.getAdapter().getCount(); i++) {
+            if (checked.get(i)){
                 selecteds.add(allPoints.get(i));
             }
         }
@@ -85,4 +100,65 @@ public class FilterActivity extends Activity {
         return selecteds;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.acceptButton:
+                accept();
+                break;
+            case R.id.cancelButton:
+                cancel();
+                break;
+            case R.id.selectButton:
+                selectAll();
+                break;
+            case R.id.deselectButton:
+                deselectAll();
+                break;
+            default:
+                Log.e(TAG, "Unregistered event");
+                break;
+        }
+    }
+
+    /**
+     * Finishes the activity and sets result as a list of selected {@code PointsOfInterest}'s IDs.
+     */
+    private void accept() {
+        Intent returnIntent = new Intent();
+        returnIntent.putStringArrayListExtra("selected", (ArrayList<String>) getSelectedPoints());
+        setResult(RESULT_OK, returnIntent);
+        finish();
+    }
+
+    /**
+     * Finishes the activity without changing anything.
+     */
+    private void cancel() {
+        finish();
+    }
+
+    /**
+     * Checks all items inside the list view
+     */
+    private void selectAll() {
+        final ListView itemsList = (ListView) findViewById(R.id.itemsList);
+
+        for(int i = 0; i < itemsList.getAdapter().getCount(); i++) {
+            Log.e(TAG, "Item: " + i);
+            itemsList.setItemChecked(i, true);
+        }
+
+    }
+
+    /**
+     * Eliminates all checks inside the list view
+     */
+    private void deselectAll() {
+        final ListView itemsList = (ListView) findViewById(R.id.itemsList);
+
+        for(int i = 0; i < itemsList.getAdapter().getCount(); i++) {
+            itemsList.setItemChecked(i, false);
+        }
+    }
 }
