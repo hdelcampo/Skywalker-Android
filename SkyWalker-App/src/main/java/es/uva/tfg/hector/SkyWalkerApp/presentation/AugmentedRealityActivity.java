6b3e7df3b7300f263a46615773afae6594048d79
FoxeyYy <hectorcampop@outlook.com>
2017-04-04 10:12:41 +0200

    Fixed full screen implementation for AR activity

--- SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/presentation/AugmentedRealityActivity.java
+++ SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/presentation/AugmentedRealityActivity.java
@@ -2,6 +2,7 @@ package es.uva.tfg.hector.SkyWalkerApp.presentation;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -16,6 +17,18 @@ import es.uva.tfg.hector.SkyWalkerApp.business.PointOfInterest;
 public class AugmentedRealityActivity extends FragmentActivity
         implements AugmentedRealityControlsFragment.AugmentedRealityControls {
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
