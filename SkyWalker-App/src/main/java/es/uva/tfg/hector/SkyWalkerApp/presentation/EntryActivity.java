08dcaf97c2d17366919d94cc9361feeb9c097a41
Héctor <hectorcampop@outlook.com>
2017-04-19 21:42:15 +0200

    Initial version of pages

--- SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/presentation/EntryActivity.java
+++ SkyWalker-App/src/main/java/es/uva/tfg/hector/SkyWalkerApp/presentation/EntryActivity.java
@@ -8,7 +8,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -54,24 +56,20 @@ public class EntryActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        private static final int NUM_PAGES = 2;
+        /**
+         * The number of pages to show.
+         */
+        private static final int NUM_PAGES = 3;
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return new AROnBoardingFragment();
-                case 1:
-                    return new AROnBoardingFragment();
-                default:
-                    return null;
-            }
+            return OnBoardingPageFragment.newInstance(position);
         }
 
         @Override
@@ -80,5 +78,52 @@ public class EntryActivity extends AppCompatActivity {
         }
 
     }
+
+    /**
+     * A On-Boarding page, position determines the content.
+     */
+    public static class OnBoardingPageFragment extends Fragment {
+
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         * @param sectionNumber The page index
+         */
+        public static OnBoardingPageFragment newInstance(int sectionNumber) {
+            OnBoardingPageFragment fragment = new OnBoardingPageFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = null;
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 0:
+                    rootView = inflater.inflate(R.layout.ar_on_boarding_page, container, false);
+                    break;
+                case 1:
+                    rootView = inflater.inflate(R.layout.beacon_on_boarding_page, container, false);
+                    break;
+                case 2:
+                    rootView = inflater.inflate(R.layout.indoor_outdoor_on_boarding_page, container, false);
+                    break;
+            }
+
+            return rootView;
+        }
+
+    }
+
 
 }
